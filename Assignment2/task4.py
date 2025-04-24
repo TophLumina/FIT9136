1af3612662daae54a149e05b2faf148fa59f2742
Toph Lumina <2625792753@qq.com>
2025-04-24 12:22:02 +1000

    t3 t4 update

--- Assignment2/task4.py
+++ Assignment2/task4.py
@@ -5,22 +5,32 @@ cleaning_space = [
     [True, True, True, True, True, True, True, True, True, True],
     [True, True, True, True, True, True, True, True, True, True],
     [True, True, False, True, True, False, True, True, True, True],
-    #          ^^^^^           ^^^^^
+    #          ^^^^^            ^^^^^
     [True, True, True, True, True, True, True, True, True, True],
     [True, True, True, True, False, True, True, True, True, True],
-    #                    ^^^^^
+    #                     ^^^^^
     [True, True, True, True, True, True, True, True, True, True],
     [True, True, True, True, True, True, True, True, True, True],
     [True, True, True, True, True, True, True, True, True, True],
+]
+
+obstruction_space = [
+    [None, None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, "c", None, None, None, None, None],
+    [None, None, "r", None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, "w", None, None, None],
+    [None, None, None, None, None, None, None, None, None, None],
+    [None, "w", None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None, None],
 ]
 
 
 def vacuum_action(vacuum: list, action: str) -> None:
     DIRECTIONS = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
     MOVEMENTS = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
-
-    def in_area(x: int, y: int) -> bool:
-        return y < len(cleaning_space) and y >= 0 and x < len(cleaning_space[y]) and x >= 0
 
     def turn_left() -> None:
         vacuum[2] = DIRECTIONS[(DIRECTIONS.index(vacuum[2]) - 1) % len(DIRECTIONS)]
@@ -38,9 +48,21 @@ def vacuum_action(vacuum: list, action: str) -> None:
         ]
 
         # out of bound
-        if not in_area(vacuum[1], vacuum[0]):
+        if not (
+            next_pos[0] >= 0
+            and next_pos[0] < len(cleaning_space)
+            and next_pos[1] >= 0
+            and next_pos[1] < len(cleaning_space[next_pos[0]])
+        ):
             turn_right()
             return
+
+        # cat encountered
+        if obstruction_space[next_pos[0]][next_pos[1]] == "c":
+            if not obstruction_space[
+                next_pos[0] + MOVEMENTS[DIRECTIONS.index(vacuum[2])][0]
+            ][next_pos[1] + MOVEMENTS[DIRECTIONS.index(vacuum[2])][1]]:
+                pass
 
         if not cleaning_space[vacuum[0]][vacuum[1]]:
             cleaning_space[next_pos[0]][next_pos[1]] = False
@@ -66,13 +88,14 @@ def perform_cleaning(instructions: str, vacuum: list) -> None:
 # THIS ENSURES THAT THE CODE BELLOW ONLY RUNS WHEN YOU HIT THE GREEN `Run` BUTTON, AND NOT THE BLUE `Test` BUTTON
 if __name__ == "__main__":
     test_commands = "test_commands.txt"
+    test_log = "test_log.txt"
     vacuum = [2, 2, "N"]
 
     print("INITIAL SPACE")
     for row_index, row in enumerate(cleaning_space):
         for col_index, cell in enumerate(row):
-            if (row_index, col_index) == (vacuum[0], vacuum[1]):
-                print("r", end="")
+            if obstruction_space[row_index][col_index] is not None:
+                print(obstruction_space[row_index][col_index], end="")
             elif cell:
                 print(".", end="")
             else:
@@ -80,15 +103,19 @@ if __name__ == "__main__":
         print()
 
     print("CLEANING")
-    perform_cleaning(test_commands, vacuum)
+    perform_cleaning(test_commands, vacuum, test_log)
 
     print("FINAL SPACE")
     for row_index, row in enumerate(cleaning_space):
         for col_index, cell in enumerate(row):
-            if (row_index, col_index) == (vacuum[0], vacuum[1]):
-                print("r", end="")
+            if obstruction_space[row_index][col_index] is not None:
+                print(obstruction_space[row_index][col_index], end="")
             elif cell:
                 print(".", end="")
             else:
                 print("d", end="")
         print()
+
+    print("ACTIONS")
+    with open(test_log, "r") as log:
+        print(log.read())
